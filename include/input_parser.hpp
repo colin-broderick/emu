5230845b935efa0ce4285c61a3cd94ffdfa04f8a
Colin Broderick <colin.a.broderick@gmail.com>
2021-07-31 21:41:51 +0100

    Added proper input parsing

---
+++ include/input_parser.hpp
@@ -0,0 +1,56 @@
+#ifndef INPUT_PARSER_H
+#define INPUT_PARSER_H
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class InputParser
+{
+    private:
+        std::vector<std::string> tokens;
+
+    public:
+        /** \brief Constructor.
+         * \param argc Number of command line arguments received.
+         * \param argv All command line arguments.
+         */
+        InputParser(int &argc, char **argv)
+        {
+            // Add all tokens in command line to tokens vector.
+            for (int i=1; i < argc; ++i)
+            {
+                this->tokens.push_back(std::string(argv[i]));
+            }
+        }
+        
+        /** \brief Get value of a specified command line option.
+         * \param option The name of the sought parameter.
+         * \return The value of the sought parameter.
+         */
+        const std::string& getCmdOption(const std::string &option) const
+        {
+            // Identify the position of a requested option and return the next token if found.
+            std::vector<std::string>::const_iterator itr;
+            itr = std::find(this->tokens.begin(), this->tokens.end(), option);
+            if (itr != this->tokens.end() && ++itr != this->tokens.end())
+            {
+                return *itr;
+            }
+            static const std::string empty_string("");
+            return empty_string;
+        }
+        
+        /** \brief Check whether input commands containg specified string.
+         * \param option The parameter to check for.
+         * \return True if the parameter is found, false otherwise.
+         */
+        bool contains(const std::string &option) const
+        {
+            // Check for the presense of a sought option.
+            return std::find(this->tokens.begin(), this->tokens.end(), option)
+                != this->tokens.end();
+        }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,53 @@
+#include <sstream>
+
 #include "system.hpp"
+#include "input_parser.hpp"
 
 /** \brief Application entry point. Creates a NES system and executes a loaded program. */
 int main(int argc, char* argv[])
 {
-    // Check whether ROM file name is provided, and quit if not.
-    // TODO Would like proper input parser, but wanted to keep it simple and understandble for now.
-    if (argc < 2)
+    System NES;
+    
+    InputParser input{argc, argv};
+    if (input.contains("-h") || input.contains("-help") || !input.contains("-r"))
+    {
+        // TODO Add proper help text, then quit.
+        std::cout << "Usage:" << std::endl;
+        std::cout << "  -r    Path to ROM file" << std::endl;
+        std::cout << "  -ip   Specify the starting instruction pointer" << std::endl;
+        std::cout << "  -sp   Specify the starting stack pointer" << std::endl;
+        return 0;
+    }
+
+    // Get ROM file name from arguments.
+    if (input.contains("-r"))
+    {
+        std::string rom_file_name = input.getCmdOption("-r");
+        NES.load_rom(rom_file_name);
+    }
+    else
     {
         std::cout << "No ROM provided" << std::endl;
         return 0;
     }
-    std::string rom_file_name = argv[1];
-    
-    System NES;
-    NES.load_rom(rom_file_name);
-    NES.cpu.setIP(0x400);
+
+    // Check for and set stack pointer.
+    if (input.contains("-sp"))
+    {
+        Word sp;
+        std::istringstream(input.getCmdOption("-sp")) >> std::hex >> sp;
+        NES.cpu.setSP(sp);
+    }
+
+    // Check for and set instruction pointer.
+    if (input.contains("-ip"))
+    {
+        Word ip;
+        std::istringstream(input.getCmdOption("-ip")) >> std::hex >> ip;
+        NES.cpu.setIP(ip);
+    }
+
+    std::cout << "SP:" << (int)NES.cpu.getSP() << std::endl;
     NES.run();
 
     return 0;
